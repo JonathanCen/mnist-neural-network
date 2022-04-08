a13c714803dd6608d7710ede9a1e639736a4c8fd
JonathanCen <jcen3@binghamton.edu>
2022-04-08 19:38:11 -0400

    Fixed bugs with forward propagation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import platform
 import sys
 import numpy as np
 import random
-import timer
+import time
 
 import gzip
 
@@ -23,13 +23,18 @@ class Neuron:
         pass
 
 class InputLayer:
-    def __init__(self, next_layer = None) -> None:
+    def __init__(self, n_neurons, next_layer = None) -> None:
+        self.n_neurons = n_neurons
         self.next_layer = next_layer
     
-    def fowardPropagation(self, image, c_label = None, testing = False) -> None:
-        self.next_layer.fowardPropagation(image.T, c_label, testing)
+    def forward_propagation(self, image, c_label = None, testing = False) -> None:
+        # start = time.perf_counter()
+        image = image.reshape((self.n_neurons, 1))
+        # end = time.perf_counter()
+        # print(f'Reshaping image: {end - start}')
+        return self.next_layer.forward_propagation(image, c_label, testing)
 
-    def backwardPropagation(self, backward_pass_data) -> None:
+    def backward_propagation(self, backward_pass_data) -> None:
         return
 
 class FullyConnectedLayer:
@@ -43,23 +48,26 @@ class FullyConnectedLayer:
         self.weights = np.random.rand(n_neurons, n_incoming_neurons) - .5
         self.bias = np.random.rand(n_neurons, 1) - .5
         droupoutRateLambda = lambda x: 1/0.6 if x < 0.6 else 0
-        self.dropout = droupoutRateLambda(np.random.rand(n_neurons, 1))
+        self.dropout = np.array(list(map(droupoutRateLambda, np.random.rand(n_neurons)))).reshape((n_neurons,1))
 
-        self.avg_weights_deriv = np.zeros((n_neurons, n_incoming_neurons), dtype=float32)
-        self.avg_bias_deriv = np.zeros((n_neurons, 1), dtype=float32)
+        self.avg_weights_deriv = np.zeros((n_neurons, n_incoming_neurons), dtype=np.float64)
+        self.avg_bias_deriv = np.zeros((n_neurons, 1), dtype=np.float64)
         self.forward_pass_data = None
 
-    def ReLUActivationFunction(self, output):
+    def ReLU_activation_function(self, output):
         return np.maximum(0, output)
 
-    def forwardPropagation(self, forward_pass_data, c_label, testing) -> None:
+    def forward_propagation(self, forward_pass_data, c_label, testing) -> None:
+        # start_time = time.perf_counter()
         self.forward_pass_data = forward_pass_data
-        output = ReLUActivationFunction(np.matmul(self.weights, forward_pass_data) + self.bias)
-        # output = ReLUActivationFunction(np.matmul(self.weights, forward_pass_data) + self.bias) * self.dropout # can see the performance with dropout
-        prediction = self.next_layer.forwardPropagation(output, c_label, testing)
+        output = self.ReLU_activation_function(np.matmul(self.weights, forward_pass_data) + self.bias)
+        # end_time = time.perf_counter()
+        # print(f'FullyConnectedLayer {1 if self.n_neurons == 1024 else 2} took: {end_time - start_time}')
+        # output = self.ReLU_activation_function(np.matmul(self.weights, forward_pass_data) + self.bias) * self.dropout # can see the performance with dropout
+        prediction = self.next_layer.forward_propagation(output, c_label, testing)
         return prediction
 
-    def backwardPropagation(self, backward_pass_data) -> None:
+    def backward_propagation(self, backward_pass_data) -> None:
         downstream_deriv = (np.matmul(backward_pass_data.T, self.weights)).T
         # downstream_deriv = np.matmul((self.dropout * backward_pass_data).T, self.weights)
         self.avg_weights_deriv += np.matmul(backward_pass_data, self.forward_pass_data.T) / 100
@@ -68,11 +76,11 @@ class FullyConnectedLayer:
         #self.avg_bias_deriv += ((backward_pass_data * self.dropout)/100)
         self.previous_layer(downstream_deriv)
 
-    def updateWeightsAndBias(self, learning_rate) -> None:
+    def update_weights_and_bias(self, learning_rate) -> None:
         self.weights -= learning_rate * self.avg_weights_deriv
         self.bias -= learning_rate * self.avg_bias_deriv
-        self.avg_weights_deriv = np.zeros((n_neurons, n_incoming_neurons), dtype=float32)
-        self.avg_bias_deriv = np.zeros((n_neurons, 1), dtype=float32)
+        self.avg_weights_deriv = np.zeros((n_neurons, n_incoming_neurons), dtype=np.float64)
+        self.avg_bias_deriv = np.zeros((n_neurons, 1), dtype=np.float64)
 
 
 class SoftMaxLayer:
@@ -81,19 +89,26 @@ class SoftMaxLayer:
         self.next_layer = next_layer
         self.previous_layer = previous_layer
         self.forward_pass_output = None
-    
-    def forwardPropagation(self, forward_pass_data, c_label, testing) -> None:
-        """ # This is the formula I had when doing CUDA
-        intermediate_output = np.exp(output - max(forward_pass_data))
-        output = intermediate_output / sum(intermediate_output)
-        """
-        self.forward_pass_output = np.exp(forward_pass_data) / sum(np.exp(forward_pass_data))
-        if not testing:
-            self.next_layer.forwardPropagation(self.forward_pass_output, c_label)
-        return self.forward_pass_output
 
-    def backwardPropagation(self, backward_pass_data) -> None:
-        downstream_deriv = np.zeros((self.n_neurons, 1), dtype=float32)
+    def extract_prediction(self):
+        return self.forward_pass_output.argmax()
+    
+    def forward_propagation(self, forward_pass_data, c_label, testing) -> None:
+        """ # This is the formula I had when doing CUDA
+        # self.forward_pass_output = np.exp(forward_pass_data) / sum(np.exp(forward_pass_data))
+        # print(self.forward_pass_output)/
+        """
+        # start_time = time.perf_counter()
+        intermediate_output = np.exp(forward_pass_data - max(forward_pass_data))
+        self.forward_pass_output = intermediate_output / sum(intermediate_output)
+        # end_time = time.perf_counter()
+        # print(f'SoftMaxLayer took: {end_time - start_time}')
+        if not testing:
+            self.next_layer.forward_propagation(self.forward_pass_output, c_label)
+        return self.extract_prediction()
+
+    def backward_propagation(self, backward_pass_data) -> None:
+        downstream_deriv = np.zeros((self.n_neurons, 1), dtype=np.float64)
         for i in range(self.n_neurons):
             accum = backward_pass_data[i][0] * self.forward_pass_output[i] * (1 - self.forward_pass_output[i])
             forward_pass_output_remove_ith_column = np.delete(self.forward_pass_output, i, 1)
@@ -108,22 +123,27 @@ class CrossEntropyLayer:
         self.n_neurons = n_neurons
         self.previous_layer = previous_layer
 
-    def forwardPropagation(self, forward_pass_data, c_label) -> None:
-        downstream_deriv = np.zeros((n_neurons, 1), dtype=float32)
+    def forward_propagation(self, forward_pass_data, c_label) -> None:
+        downstream_deriv = np.zeros((self.n_neurons, 1), dtype=np.float64)
         downstream_deriv[c_label][0] = -1/forward_pass_data[c_label][0]
-        self.previous_layer.backwardPropagation(downstream_deriv)
+        self.previous_layer.backward_propagation(downstream_deriv)
 
 def read_int(file_pointer: gzip.GzipFile) -> int:  
     """ Returns the int of the first 4 bytes
     """
     return int.from_bytes(file_pointer.read(4), "big")
 
-def read_training_data(training_image_path: str, training_label_path: str) -> None:
+def normalize_images(images, n_neurons) -> np.ndarray:
+    normalizerLambda = lambda x: (x/127.5) - 1
+    return np.array(list(map(normalizerLambda, images)))
+
+def read_training_data() -> (np.ndarray, np.ndarray):
     """ Returns a numpy array of the training images and labels
     """
-    return loadlocal_mnist(
-        images_path='train-images-idx3-ubyte', 
-        labels_path='train-labels-idx1-ubyte')
+    training_images, training_labels = loadlocal_mnist(
+        images_path='./mnist/train-images-idx3-ubyte', 
+        labels_path='./mnist/train-labels-idx1-ubyte')
+    return normalize_images(training_images, 784 * 784), training_labels
 
 def read_training_data_slow(training_image_path: str, training_label_path: str) -> None:
     # Process the training images
@@ -167,7 +187,7 @@ def connect_layers(list_layers) -> None:
 
 def train_neural_network(training_images, training_labels, epochs) -> None:
     # Construct the Neural Network
-    input_layer = InputLayer()
+    input_layer = InputLayer(784)
     first_fully_connected_layer = FullyConnectedLayer(1024, 784)
     output_layer = FullyConnectedLayer(10, 1024)
     softmax_layer = SoftMaxLayer(10)
@@ -176,50 +196,68 @@ def train_neural_network(training_images, training_labels, epochs) -> None:
 
     # Connect the layers
     connect_layers(list_layers)
+    start_time = time.perf_counter()
+    n_corrects = 0
+    for iter in range(10000):
+        random_test = random.randint(0, training_images.shape[0]-1)
+        prediction = input_layer.forward_propagation(training_images[random_test], None, True)
+        # if iter < 10:
+            # print(f'Model\'s prediction {prediction} and the expected {training_labels[random_test]}')
+        if prediction == training_labels[random_test]: n_corrects += 1
+    print(f"Epoch 1: Round 1: accuracy={(n_corrects/10000.0):0.6f}\n")
+    end_time = time.perf_counter()
+    print(f"This test took: {(end_time - start_time):0.6f}\n")
 
-
+    """
     for epoch in range(epochs):
-        print(f"\n----------------------- STARTING EPOCH {epoch} -------------------\n";
+        print(f"\n----------------------- STARTING EPOCH {epoch} -------------------\n")
         start_time = time.perf_counter()
 
         for c_round in range(600):
             # test the neural network every 100 rounds for each epoch
             if c_round % 100 == 0:
-                corrects = 0
+                n_corrects = 0
                 for iter in range(10000):
                     random_test = random.randint(0, training_images.shape[0])
-                    prediction = input_layer.fowardPropagation(training_images[index], None, True)
-                    if prediction == training_labels[random_test]: corrects += 1
-                print(f"Epoch {epoch}: Round {c_round:5d}: accuracy={(correct/10000.0):0.6f}\n")
+                    prediction = input_layer.forward_propagation(training_images[random_test], None, True)
+                    if prediction == training_labels[random_test]: n_corrects += 1
+                print(f"Epoch {epoch}: Round {c_round:5d}: accuracy={(n_corrects/10000.0):0.6f}\n")
             
             # continue to train the neural network
             for iter in range(100):
                 index = (c_round * 100) + iter
-                input_layer.fowardPropagation(training_images[index], training_labels[index])
+                input_layer.forward_propagation(training_images[index], training_labels[index])
 
         end_time = time.perf_counter()
         print(f"This epoch took: {(end_time - start_time):0.6f}\n")
+        """
 
 
 
 
 
-
-def main() None:
+def main() -> None:
     """
     if (len(sys.argv) < 5):
         print("Too few args. Pass in ./program training_image_file training_label_file test_image_file test_label_file")
         return 
     """
     
-    training_image_path, training_label_path, epochs = sys.argv[1], sys.argv[2], 20
-
+    # training_image_path, training_label_path, epochs = sys.argv[1], sys.argv[2], 20
+    epochs = 20
+    
+    start = time.perf_counter()
     # Read in training data
-    training_images, training_labels = read_training_data(training_image_path, training_label_path)
-    print("Finished reading all the images into the program.")
-
+    print("Starting to read training data for the neural network")
+    # training_images, training_labels = read_training_data(training_image_path, training_label_path)
+    training_images, training_labels = read_training_data()
+    end = time.perf_counter()
+    print(f"Finished reading all the images into the program, in {end - start} seconds")
+    
     # Train the Neural Network
-    train_neural_network(training_images, training_labels)
+    print("Starting to train the neural network")
+    train_neural_network(training_images, training_labels, epochs)
+    print("Finish training the neural network")
 
     # Test the Neural Network
 
